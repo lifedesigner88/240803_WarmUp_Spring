08881a3cbc4ac3172b795ebcba4dea4a8b384007
sejongPark <lifedesigner88@gmail.com>
2024-08-03 16:42:19 +0900

    DeleteController

---
+++ src/main/java/org/example/hello2/controller/DeleteController.java
@@ -0,0 +1,18 @@
+package org.example.hello2.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/delete-api")
+public class DeleteController {
+
+    @DeleteMapping("{variable}")
+    public String DeleteVariable(@PathVariable String variable) {
+        return variable;
+    }
+
+    @DeleteMapping("delete")
+    public String DeleteByEmail(@RequestParam String email){
+        return "email" +" : " + email;
+    }
+}
--- src/main/java/org/example/hello2/controller/PutController.java
+++ src/main/java/org/example/hello2/controller/PutController.java
@@ -1,13 +1,14 @@
 package org.example.hello2.controller;
 
 import org.example.hello2.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("api/v1/put-api")
@@ -26,5 +27,13 @@ public class PutController {
     @PutMapping("member1")
     public MemberDto putMemberDto1(@RequestBody MemberDto memberDto) {
         return memberDto;
+    }
+
+    @PutMapping("member3")
+    public ResponseEntity<MemberDto> putMemberDto3(
+            @RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 }
