62546e5e1d251a7eefc9878242756b469078c3e5
sejongPark <lifedesigner88@gmail.com>
2024-08-03 13:06:45 +0900

    first commint

--- src/main/java/org/example/hello2/Hello2Application.java
+++ src/main/java/org/example/hello2/Hello2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Hello2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Hello2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Hello2Application.class, args);
+    }
 
 }
--- src/main/java/org/example/hello2/controller/GetController.java
+++ src/main/java/org/example/hello2/controller/GetController.java
@@ -1,12 +1,21 @@
 package org.example.hello2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/get-api")
 public class GetController {
+
+    @GetMapping("name")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("variable1/{variable}")
+    public String variable1(@PathVariable String variable) {
+        return variable;
     }
 }
