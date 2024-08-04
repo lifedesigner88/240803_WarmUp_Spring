009abbe0ef00c68b132d18ef1ecd9405aaa29176
sejongPark <lifedesigner88@gmail.com>
2024-08-04 10:13:55 +0900

    Log 출력 테스트

--- src/main/java/org/example/hello2/controller/GetController.java
+++ src/main/java/org/example/hello2/controller/GetController.java
@@ -3,19 +3,26 @@ package org.example.hello2.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.example.hello2.dto.MemberDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @GetMapping("name")
     public String hello() {
+        LOGGER.info("Hello World 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
     @GetMapping("variable1/{variable}")
     public String variable1(@PathVariable String variable) {
+        LOGGER.info("변수값 입니다.{}", variable);
         return variable;
     }
 
